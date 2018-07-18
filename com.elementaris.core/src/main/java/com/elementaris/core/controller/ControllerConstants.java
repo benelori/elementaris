a34ed675b301110590c35889ff3f5a9e96729beb
Arctigor <Arctigor@DESKTOP-03IV68I>
2018-07-18 23:19:28 +0300

    Add more test template.

--- com.elementaris.core/src/main/java/com/elementaris/core/controller/ControllerConstants.java
+++ com.elementaris.core/src/main/java/com/elementaris/core/controller/ControllerConstants.java
@@ -3,4 +3,5 @@ package com.elementaris.core.controller;
 public final class ControllerConstants {
 	// pages
 	public final static String ADMIN = "secretstuff";
+	public final static String ADMIN_AI = "ai";
 }
--- com.elementaris.core/src/main/java/com/elementaris/core/controller/admin/AdminController.java
+++ com.elementaris.core/src/main/java/com/elementaris/core/controller/admin/AdminController.java
@@ -9,13 +9,24 @@ import com.elementaris.core.controller.ControllerConstants;
 
 @Controller
 public class AdminController {
+	private static final String VIEW_PREFIX = "admin/";
 	private final static String ADMIN_MAPPING = "/" + ControllerConstants.ADMIN;
+	private final static String ADMIN_VIEW = VIEW_PREFIX + ControllerConstants.ADMIN;
+	private final static String ADMIN_AI_MAPPING = ADMIN_MAPPING + "/" + ControllerConstants.ADMIN_AI;
+	private final static String ADMIN_AI_VIEW = VIEW_PREFIX + "/" + ControllerConstants.ADMIN_AI;
 
 	@GetMapping(ADMIN_MAPPING)
-	public String admin(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+	public String adminHomePage(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
 			Model model) {
 		model.addAttribute("name", name);
-		return ControllerConstants.ADMIN;
+		return ADMIN_VIEW;
+	}
+
+	@GetMapping(ADMIN_AI_MAPPING)
+	public String adminAIHomePage(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return ADMIN_AI_VIEW;
 	}
 
 }
