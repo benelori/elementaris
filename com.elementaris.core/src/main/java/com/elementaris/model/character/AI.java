6041098c56db0410d0dcfd4c5b629ac2297a93b5
Bene, Szabolcs <szabolcs.bene@aaccenture.com>
2018-07-18 15:44:47 +0300

    Add basic characters.

---
+++ com.elementaris.core/src/main/java/com/elementaris/model/character/AI.java
@@ -0,0 +1,16 @@
+package com.elementaris.model.character;
+
+import game.core.model.character.AbstractCharacter;
+
+public class AI extends AbstractCharacter {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ com.elementaris.core/src/main/java/com/elementaris/model/character/User.java
@@ -0,0 +1,25 @@
+package com.elementaris.model.character;
+
+import game.core.model.character.AbstractCharacter;
+
+public class User extends AbstractCharacter {
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
