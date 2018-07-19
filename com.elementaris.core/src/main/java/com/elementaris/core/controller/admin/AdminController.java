d209e3df5d923b2f2a9edd7e46222fb0617ddc3f
Bene, Szabolcs <szabolcs.bene@aaccenture.com>
2018-07-19 17:33:47 +0300

    Added links to controllers/views

--- com.elementaris.core/src/main/java/com/elementaris/core/controller/admin/AdminController.java
+++ com.elementaris.core/src/main/java/com/elementaris/core/controller/admin/AdminController.java
@@ -1,32 +1,37 @@
 package com.elementaris.core.controller.admin;
 
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.elementaris.core.controller.ControllerConstants;
 
 @Controller
 public class AdminController {
-	private static final String VIEW_PREFIX = "admin/";
+	private static final String ADMIN_VIEW_PREFIX = "admin/";
 	private final static String ADMIN_MAPPING = "/" + ControllerConstants.ADMIN;
-	private final static String ADMIN_VIEW = VIEW_PREFIX + ControllerConstants.ADMIN;
+	private final static String ADMIN_VIEW = ADMIN_VIEW_PREFIX + ControllerConstants.ADMIN;
 	private final static String ADMIN_AI_MAPPING = ADMIN_MAPPING + "/" + ControllerConstants.ADMIN_AI;
-	private final static String ADMIN_AI_VIEW = VIEW_PREFIX + "/" + ControllerConstants.ADMIN_AI;
-
-	@GetMapping(ADMIN_MAPPING)
-	public String adminHomePage(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
-		return ADMIN_VIEW;
-	}
+	private final static String ADMIN_AI_VIEW = ADMIN_VIEW_PREFIX + "/" + ControllerConstants.ADMIN_AI;
+	private final static String ADMIN_TECHNIQUE_MAPPING = ADMIN_MAPPING + "/" + ControllerConstants.ADMIN_TECHNIQUE;
+	private final static String ADMIN_TECHNIQUE_VIEW = ADMIN_VIEW_PREFIX + "/" + ControllerConstants.ADMIN_TECHNIQUE;
 
 	@GetMapping(ADMIN_AI_MAPPING)
-	public String adminAIHomePage(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
+	public String adminAI(Model model) {
 		return ADMIN_AI_VIEW;
+	}
+
+	@GetMapping(ADMIN_TECHNIQUE_MAPPING)
+	public String adminTechnique(Model model) {
+		return ADMIN_TECHNIQUE_VIEW;
+	}
+
+	@GetMapping(ADMIN_MAPPING)
+	public String admin(Model model) {
+		model.addAttribute("selectedViews", Arrays.asList("a", "b"));
+		return ADMIN_VIEW;
 	}
 
 }
